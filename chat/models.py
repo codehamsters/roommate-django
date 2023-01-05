73bbbca61e15a05a178883a3861a3796c6981973
codehamsters <rohandeepbansal@gmail.com>
2023-01-05 23:07:39 +0530

    Room name Done

--- chat/models.py
+++ chat/models.py
@@ -1,7 +1,8 @@
 from django.db import models
-
+from datetime import datetime
 # Create your models here.
 class Room(models.Model):
-    name=models.CharField(max_length=100)
+    name=models.CharField(max_length=15)
+    time=models.TimeField(default=datetime.now, blank=True)
 class Name(models.Model):
     name=models.CharField(max_length=100)
--- chat/views.py
+++ chat/views.py
@@ -1,9 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse, JsonResponse
 from .models import Room
 
 # Create your views here.
 
 def home(request):
-    rooms=Room.objects.all()
+    rooms=reversed(Room.objects.all())
     return render(request, 'main.html', {'rooms': rooms})
+def create(request):
+    import re
+    room=request.POST["room-name-input"]
+    allowed= r'^[_. A-Z0-9a-z]*$'
+    if re.match(allowed, room)==None or len(room)>25:
+        return redirect('/')
+    else:
+        if Room.objects.filter(name=room).exists():
+            return redirect('/')
+        else:
+            new_room=Room.objects.create(name=room)
+            new_room.save()
+            return redirect('/')
