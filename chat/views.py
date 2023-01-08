95a944990f6fa78655a4d2a0b5fda4ae36e47529
codehamsters <rohandeepbansal@gmail.com>
2023-01-08 16:29:36 +0530

    ok

--- chat/views.py
+++ chat/views.py
@@ -10,6 +10,7 @@ def home(request):
 def create(request):
     import re
     room=request.POST["room-name-input"]
+    room=room.replace("/",'//')
     allowed= r'^[_. A-Z0-9a-z]*$'
     if re.match(allowed, room)==None or len(room)>25:
         return redirect('/')
