71c43f64740c1dd06cc2898c962fe379e67e606d
codehamsters <rohandeepbansal@gmail.com>
2023-01-08 16:45:34 +0530

    hyphen allowed

--- chat/views.py
+++ chat/views.py
@@ -11,7 +11,7 @@ def create(request):
     import re
     room=request.POST["room-name-input"]
     room=room.replace("/",'//')
-    allowed= r'^[_. A-Z0-9a-z]*$'
+    allowed= r'^[_.- A-Z0-9a-z]*$'
     if re.match(allowed, room)==None or len(room)>25:
         return redirect('/')
     else:
